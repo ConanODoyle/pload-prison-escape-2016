c58557ed551fd3aaf11060d2e5963d1260a9bd86
ConanODoyle <this.is.not.a.email.account@gmail.com>
2016-08-09 20:24:21 -0700

    Remove unnecessary bot hole addon references in bot_dog

--- Bot_Dog/Bot_Horse/server.cs
+++
@@ -1,4 +0,0 @@
-//Base Blockhead guy
-if(ForceRequiredAddOn("Bot_Hole") == $Error::None &&
-   ForceRequiredAddOn("Vehicle_Horse") == $Error::None )
-	exec("./bot_base.cs");
--- Bot_Dog/Bot_Zombie/server.cs
+++
@@ -1,3 +0,0 @@
-//Base Blockhead guy
-if(LoadRequiredAddOn("Bot_Hole") == $Error::None)
-	exec("./bot_zombie.cs");
